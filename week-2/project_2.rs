15617d5207fc434598e56ee140a992ca37a1d8fb
Eniibukun Olalandu <eniibukun.olalandu@pau.edu.ng>
2024-10-26 17:52:32 -0400

    (My second commit)

---
+++ week-2/project_2.rs
@@ -0,0 +1,14 @@
+fn main() {
+	let a:f64 = 450_000.0;
+	let b:f64 = 1_500_000.0;
+	let c:f64 = 750_000.0;
+	let d:f64 = 2_850_000.0;
+	let e:f64 = 250_000.0;
+
+	//sum
+	let s = a + b + c + d + e;
+	println!("Sum is {}", a);
+	let q = 2.0 + 1.0 + 3.0 + 3.0 + 1.0;
+	let m = s / q;
+	println!("Average is {}", m);
+}
