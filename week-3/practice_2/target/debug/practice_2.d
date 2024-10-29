a406d2d55d762243cf6d2041d72680bfed00866c
Eniibukun Olalandu <eniibukun.olalandu@pau.edu.ng>
2024-10-29 16:02:09 -0400

    (my third commit)

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-3\practice_6\target\debug\practice_6.exe: C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-3\practice_6\src\main.rs
