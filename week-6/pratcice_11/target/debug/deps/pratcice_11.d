985d3ddb7faa592e8bb2619a7c0c52aa8134e9ed
Eniibukun Olalandu <eniibukun.olalandu@pau.edu.ng>
2024-11-23 12:38:19 +0000

    (my fifth commit)

---
+++ week-6/pratcice_11/target/debug/deps/pratcice_11.d
@@ -0,0 +1,5 @@
+C:\Users\Eniibukun Olalandu\Documents\e.olalanduCOS101\week-6\pratcice_11\target\debug\deps\pratcice_11.exe: src/main.rs
+
+C:\Users\Eniibukun Olalandu\Documents\e.olalanduCOS101\week-6\pratcice_11\target\debug\deps\pratcice_11.d: src/main.rs
+
+src/main.rs:
---
+++ week-6/pratcice_11/target/debug/pratcice_11.d
@@ -0,0 +1,1 @@
+C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-6\pratcice_11\target\debug\pratcice_11.exe: C:\Users\Eniibukun\ Olalandu\Documents\e.olalanduCOS101\week-6\pratcice_11\src\main.rs
